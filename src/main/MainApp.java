51c5cb02caa0891f2b014191a63f8cbf49e7c004
Your Name <khaoulafarrad24@gmail.com>
2017-11-27 12:51:35 +0100

    third

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -2,6 +2,6 @@ package main;
 public class MainApp {
 	public static void main(String[] args) {
 		for(int i=0;i<10;i++)
-	      System.out.println("hello khaoula"+i);
+	      System.out.println("hello khaoulaa"+i);
 	   }
 }
